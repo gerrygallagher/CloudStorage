c52d3895318283f2a98e4dad7caa0d7f467dd5f8
gerrygallagher <gpg5258@psu.edu>
2025-06-17 20:42:30 -0400

    client file added

---
+++ client.c
@@ -0,0 +1,15 @@
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+
+
+
+
+int main(){
+
+}
